a0c8743f1b7ff7479fcfe7d9a3e323d95a80a01c
Shinji Pons <hello@shinjipons.com>
2019-09-19 18:18:03 +0200

    Added project template

---
+++ page-template.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	
+	<?php include '_head.php';?>
+	
+</head>
+<body>
+
+	<?php include '_nav.php';?>
+	
+	<section class="w2">
+		<h1>Project Title</h1>
+	</section>
+	<section class="w4 no-rm">
+		<p>This is the description of the project in a few sentences. Feel free to make it as short or as long as you want.</p>
+		<p>The “organic” sources. Credibly innovate granular internal or “organic” sources whereas high standards in web-readiness. Energistically scale future-proof core competencies vis-a-vis impactful experiences. Dramatically synthesize integrated schemas with optimal networks.</p>
+	</section>
+	
+	<img src="data/img/bowie-1.jpg" class="c4">
+	<img src="data/img/bowie-1.jpg" class="c4">
+	<img src="data/img/bowie-1.jpg" class="c4">
+	<img src="data/img/bowie-1.jpg" class="c4">
+
+	<?php include '_footer.php';?>
+
+</body>
+</html>
