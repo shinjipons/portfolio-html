56c4621fb866a1cd8bc5b6652c3fba72a6beaa18
Shinji Pons <hello@shinjipons.com>
2019-09-22 19:28:53 +0200

    updated about page

--- about.php
+++ about.php
@@ -21,11 +21,42 @@
 
 	<section>
 		<section class="w2">
-			<h1>The kind of work I like doing</h1>
+			<h1>Companies I have worked for</h1>
 		</section>
 		<section class="w4 no-rm">
-			<p>Marcus W. Bradshaw is Photographer and Director based between London and New York working across multi platform projects within the fashion, beauty, celebrity and advertising industries.</p>
-			<p>The “organic” sources. Credibly innovate granular internal or “organic” sources whereas high standards in web-readiness. Energistically scale future-proof core competencies vis-a-vis impactful experiences. Dramatically synthesize integrated schemas with optimal networks.</p>
+			<ul>
+				<li>LEGO</li>
+				<li>Autodesk</li>
+				<li>Thomson Reuters</li>
+				<li>Microsoft</li>
+				<li>Style.com</li>
+				<li>Barnbrook</li>
+				<li>The Design Lab</li>
+				<li>Total Identity</li>
+				<li>Ateliers Jean Nouvel</li>
+			</ul>
+		</section>
+	</section>
+
+	<section>
+		<section class="w2">
+			<h1>I'm really good at</h1>
+		</section>
+		<section class="w4 no-rm">
+			<ul>
+				<li>Turning something good into something great</li>
+			</ul>
+		</section>
+	</section>
+
+	<section>
+		<section class="w2">
+			<h1>I'm want to be better at</h1>
+		</section>
+		<section class="w4 no-rm">
+			<ul>
+				<li>Class-A Modelling</li>
+			</ul>
 		</section>
 	</section>
 
