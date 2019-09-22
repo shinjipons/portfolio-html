c7f65ed3d5f2c669e912e49eb60ef0c75c03f712
Shinji Pons <hello@shinjipons.com>
2019-09-22 23:21:47 +0200

    cleanup menu

--- _nav.php
+++ _nav.php
@@ -3,10 +3,10 @@
 		<a href="index.php">Shinji Pons</a>
 	</p>
 	<div class="w4 no-rm">
-		<a href="index.php">Work</a>
 		<a href="archive.php">Archive</a>
 		<a href="about.php">About</a>
-		<a href="contact.php">Contact</a>
+		<a href="about.php#contact">Contact</a>
 		<a href="links.php">Links</a>
+		<a href="https://medium.com/@shinjipons" target="_blank">Medium</a>
 	</div>
 </nav>
--- about.php
+++ about.php
@@ -8,81 +8,76 @@
 <body>
 
 	<?php include '_nav.php';?>
-
-	<img src="data/img/about-001.jpg" alt="" class="p2 w2 mb">
 	
-	<section>
-		<section class="w2">
-			<h1>About me</h1>
-		</section>
-		<section class="w4 no-rm">
-			<p>Marcus W. Bradshaw is Photographer and Director based between London and New York working across multi platform projects within the fashion, beauty, celebrity and advertising industries.</p>
-			<p>The “organic” sources. Credibly innovate granular internal or “organic” sources whereas high standards in web-readiness. Energistically scale future-proof core competencies vis-a-vis impactful experiences. Dramatically synthesize integrated schemas with optimal networks.</p>
-		</section>
+	<section class="w2">
+		<h1>About me</h1>
+	</section>
+	<section class="w4 no-rm">
+		<p>Marcus W. Bradshaw is Photographer and Director based between London and New York working across multi platform projects within the fashion, beauty, celebrity and advertising industries.</p>
+		<p>The “organic” sources. Credibly innovate granular internal or “organic” sources whereas high standards in web-readiness. Energistically scale future-proof core competencies vis-a-vis impactful experiences. Dramatically synthesize integrated schemas with optimal networks.</p>
 	</section>
 
-	<section>
-		<section class="w2">
-			<h1>Companies I have worked for</h1>
-		</section>
-		<section class="w4 no-rm">
-			<ul>
-				<li>LEGO</li>
-				<li>Autodesk</li>
-				<li>Thomson Reuters</li>
-				<li>Microsoft</li>
-				<li>Style.com</li>
-				<li>Barnbrook</li>
-				<li>The Design Lab</li>
-				<li>Total Identity</li>
-				<li>Ateliers Jean Nouvel</li>
-			</ul>
-		</section>
+	<section class="w2">
+		<h1>Companies I have worked for</h1>
+	</section>
+	<section class="w4 no-rm">
+		<ul>
+			<li>LEGO</li>
+			<li>Autodesk</li>
+			<li>Thomson Reuters</li>
+			<li>Microsoft</li>
+			<li>Style.com</li>
+			<li>Barnbrook</li>
+			<li>The Design Lab</li>
+			<li>Total Identity</li>
+			<li>Ateliers Jean Nouvel</li>
+		</ul>
 	</section>
 
-	<section>
-		<section class="w2">
-			<h1>Where I studied</h1>
-		</section>
-		<section class="w4 no-rm">
-			<ul>
-				<li>London College of Communication</li>
-				<li>Coventry University</li>
-			</ul>
-		</section>
+	<section class="w2">
+		<h1>Where I studied</h1>
+	</section>
+	<section class="w4 no-rm">
+		<ul>
+			<li>London College of Communication</li>
+			<li>Coventry University</li>
+		</ul>
 	</section>
 
-	<section>
-		<section class="w2">
-			<h1>What I studied</h1>
-		</section>
-		<section class="w4 no-rm">
-			<ul>
-				<li>Graphic & Media Design</li>
-			</ul>
-		</section>
+	<section class="w2">
+		<h1>What I studied</h1>
+	</section>
+	<section class="w4 no-rm">
+		<ul>
+			<li>Graphic & Media Design</li>
+		</ul>
 	</section>
 
-	<section>
-		<section class="w2">
-			<h1>I'm really good at</h1>
-		</section>
-		<section class="w4 no-rm">
-			<ul>
-				<li>Turning something good into something great</li>
-			</ul>
-		</section>
+	<section class="w2">
+		<h1>I'm really good at</h1>
+	</section>
+	<section class="w4 no-rm">
+		<ul>
+			<li>Turning something good into something great</li>
+		</ul>
 	</section>
 
-	<section>
-		<section class="w2">
-			<h1>I want to become better at</h1>
-		</section>
-		<section class="w4 no-rm">
-			<ul>
-				<li>Class-A Modelling</li>
-			</ul>
-		</section>
+	<section class="w2">
+		<h1>I want to become better at</h1>
+	</section>
+	<section class="w4 no-rm">
+		<ul>
+			<li>Class-A Modelling</li>
+		</ul>
+	</section>
+
+	<section class="w2" id=contact>
+		<h1>Send requests to →</h1>
+	</section>
+
+	<section class="w2">
+		<p><a href="mailto:hello@shinjipons.com?subject=You%20have%20mail!">hello@shinjipons.com</a></p>
+		<p>No recruitement agencies.</p>
 	</section>
 
 	<?php include '_footer.php';?>
