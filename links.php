385f7cb1f73de0bf9211afc323bdcb6585c5e14e
Shinji Pons <streaming040404@protonmail.com>
2019-09-24 16:57:31 +0200

    added link

--- links.php
+++ links.php
@@ -14,10 +14,18 @@
 	</section>
 	<section class="w4 no-rm">
 		<ul>
-			<li><a href="https://www.youtube.com/watch?v=kqsmCUo3xEQ" target="_blank">Bitcoin: A Swiss Bank for Everyone's Pocket</a></li>
 			<li><a href="https://youtu.be/pT19VwBAqKA" target="_blank">Protecting Privacy with Math</a></li>
 			<li><a href="http://bachgarde.se/gridsystem/gridsystem.html"  target="_blank">Marcus Gärde gridsystems based on old books and scrolls</a></li>
 			<li><a href="http://www.adultswim.com/etcetera/elastic-man/" target="_blank">Elastic Man</a></li>
+		</ul>
+	</section>
+	<section class="w2">
+		<h1>Cryptocurrency</h1>
+	</section>
+	<section class="w4 no-rm">
+		<ul>
+			<li><a href="https://www.youtube.com/watch?v=kqsmCUo3xEQ" target="_blank">Bitcoin: A Swiss Bank for Everyone's Pocket</a></li>
+			<li><a href="http://galgitron.net/Post/Coin-Robbery?utm_source=share&utm_medium=ios_app&utm_name=iossmf" target="_blank">Coin Robbery</a></li>
 		</ul>
 	</section>
 
