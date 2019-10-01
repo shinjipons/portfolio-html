40514677a653fee8a720ba68378715e43fff2166
Shinji Pons <hello@shinjipons.com>
2019-10-01 21:20:38 +0200

    Update about.php

--- about.php
+++ about.php
@@ -13,8 +13,7 @@
 		<h1>About me</h1>
 	</section>
 	<section class="w3">
-		<p>Marcus W. Bradshaw is Photographer and Director based between London and New York working across multi platform projects within the fashion, beauty, celebrity and advertising industries.</p>
-		<p>The “organic” sources. Credibly innovate granular internal or “organic” sources whereas high standards in web-readiness. Energistically scale future-proof core competencies vis-a-vis impactful experiences. Dramatically synthesize integrated schemas with optimal networks.</p>
+		<p>Yet to be filled in.</p>
 	</section>
 
 	<section class="w2">
