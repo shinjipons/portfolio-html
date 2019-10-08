f23edc2e254e80d3c38d8eab54767c08b4388dff
Shinji Pons <hello@shinjipons.com>
2019-10-08 22:05:43 +0200

    preload font woff 2 file

--- _head.php
+++ _head.php
@@ -1,3 +1,4 @@
 <meta charset="UTF-8">
 <title>Shinji Pons</title>
+<link rel="preload" href="data/fonts/webfont.woff2" as="font" crossorigin="anonymous">
 <link rel="stylesheet" href="dist/css/stylesheet.css">
