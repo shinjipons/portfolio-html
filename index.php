013aebffe1c1e432cf8e4440b42cef00535a9e34
Shinji Pons <hello@shinjipons.com>
2019-10-20 22:43:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -20,13 +20,13 @@
 
 	<section>
 		<a href="louvre.php"        class="c2"><img src="data/img/louvre/louvre-cover.png"></a>
+		<a href="dynamo.php" 		class="c6"><img src="data/img/dynamo/dynamo-cover.png"></a>
 		<a href="urbantorque.php"   class="c4"><img src="data/img/urbantorque/urbantorque-004.jpg"></a>
 		<a href="modulo.php"        class="c6"><img src="data/img/modulo/modulo-cover.png"></a>
 		<a href="bowie.php"         class="c4"><img src="data/img/bowie/bowie-cover.jpg"></a>
 		<a href="basiljs.php"		class="c4"><img src="data/img/basiljs/basiljs-012.jpg"></a>
 		<a href="doty.php"          class="c2"><img src="data/img/doty/doty-cover.png"></a>
 		<!-- <a href="subd.php"          class="c4"><img src="data/img/subd/subd-cover.jpg"></a> -->
-		<a href="dynamo.php" 		class="c4"><img src="data/img/dynamo/dynamo-cover.png"></a>
 		<a href="green-thing.php"   class="c2"><img src="data/img/green-thing/green-thing-001.jpg"></a>
 		<a href="business-card.php" class="c4"><img src="data/img/business-card/business-card-001.jpg"></a>
 		<a href="clay.php"          class="c4"><img src="data/img/clay/clay-007.JPG"></a>
