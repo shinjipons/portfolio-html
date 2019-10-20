7337626e1a3a53cb0c385977d6fd5b773924cced
Shinji Pons <hello@shinjipons.com>
2019-10-20 22:33:09 +0200

    added images

---
+++ dynamo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	
+	<?php include '_head.php';?>
+	
+</head>
+<body>
+
+	<?php include '_nav.php';?>
+	
+	<section class="project-title">
+		<h1>Project Title</h1>
+	</section>
+	<section class="project-description">
+		<p>Yet to be filled.</p>
+	</section>
+	
+	<img src="data/img/bowie-1.jpg" class="c4">
+
+	<?php include '_footer.php';?>
+
+</body>
+</html>
