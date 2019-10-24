1bdd24e43c792a706de36844fe32a61b34ffe0e6
Shinji Pons <hello@shinjipons.com>
2019-10-24 19:48:12 +0200

    added device width

--- _head.php
+++ _head.php
@@ -1,4 +1,5 @@
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width">
 <title>Shinji Pons</title>
 <link rel="preload" href="data/fonts/webfont.woff2" as="font" crossorigin="anonymous">
 <link rel="stylesheet" href="dist/css/stylesheet.css">
