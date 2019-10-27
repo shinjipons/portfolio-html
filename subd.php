c4f9cfb2db99349c8aaa35d9504eb64622bb019f
Shinji Pons <hello@shinjipons.com>
2019-10-27 22:26:55 +0100

    added sub-D project page and images

---
+++ subd.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	
+	<?php include '_head.php';?>
+	
+</head>
+<body>
+
+	<?php include '_nav.php';?>
+	
+	<section class="project-title">
+		<h1>Autodesk Alias Hybrid Modelling</h1>
+	</section>
+	<section class="project-description">
+		<p>Alias 2020 industrial design software offers a unified hybrid modeling solution with integrated SUBD modeling paradigms.</p>
+		<p>Alias 2020 introduces new subdivision surface modeling tools aimed at adding speed and flexibility to your concept modeling and idea creation workflows. The addition of subdivision surfaces to Alias integrates the strengths of adaptive subdivision with native NURBS surface technology.</p>
+		<p>This means that your concept modeling can be done within a hybrid NURBS and subdivision surface modeling environment, which lets you combine NURBS editing tools and workflows with subdivision objects. Subdivision surfaces also support construction history, so your combined workflows remain fast and efficient.</p>
+	</section>
+	
+	<video controls src="data/mov/subd/subd.mp4" class="c4"></video>
+	<img src="data/img/subd-raw/subd-001.jpg" class="c4">
+	<img src="data/img/subd-raw/subd-002.jpg" class="c4">
+	<img src="data/img/subd-raw/subd-003.jpg" class="c4">
+	<img src="data/img/subd-raw/subd-004.png" class="c4">
+	<img src="data/img/subd-raw/subd-005.jpg" class="c4">
+	<img src="data/img/subd-raw/subd-006.jpg" class="c4">
+	<img src="data/img/subd-raw/subd-007.jpg" class="c4">
+	<img src="data/img/subd-raw/subd-008.png" class="c4">
+	<img src="data/img/subd-raw/subd-009.png" class="c4">
+	<img src="data/img/subd-raw/subd-010.png" class="c4">
+	<img src="data/img/subd-raw/subd-011.png" class="c4">
+	<img src="data/img/subd-raw/subd-013.jpg" class="c4">
+	<img src="data/img/subd-raw/subd-012.jpg" class="c4">
+
+	<?php include '_footer.php';?>
+
+</body>
+</html>
