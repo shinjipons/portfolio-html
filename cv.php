04a895aaab2cf372153b23462652276517da8381
John Appleseed <streaming040404@protonmail.com>
2020-02-20 18:58:18 +0100

    tried doing a CV in html

---
+++ cv.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Shinji Pons - CV</title>
+	<meta name="viewport" content="width=device-width">
+	<link rel="preload" href="data/fonts/webfont.woff2" as="font" crossorigin="anonymous">
+	<link rel="stylesheet" href="dist/css/stylesheet.css">
+</head>
+<body>
+
+	<h1>Shinji Pons</h1>
+	<section>
+		<h1>Profile</h1>
+		<p>I am a result-driven designer with a passion for science, math and technical processes to design features and tools that deliver ultimate customer satisfaction. I am also a proactive self-starter who seeks information in order to solve customer and business problems in the most efficient way.</p>
+	</section>
+	
+</body>
+</html>
