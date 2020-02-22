b779f028fa961b5e69bc404cc59cef169547548f
John Appleseed <streaming040404@protonmail.com>
2020-02-22 18:03:55 +0100

    Update -quotes.php

--- -quotes.php
+++ -quotes.php
@@ -19,12 +19,11 @@
 	</section>
 
 	<section class="w2">
-		<h1>Tools</h1>
+		<h1>Dominic Cummings - <a href="https://dominiccummings.com/2020/01/02/two-hands-are-a-lot-were-hiring-data-scientists-project-managers-policy-experts-assorted-weirdos/" target="_blank">"Two hands are a lot"</a></h1>
 	</section>
 	<section class="w4 no-rm">
 		<ul>
 			<li>The Oxford Handbook on Megaprojects points out that it is possible to quantify lessons from the failures of projects like high speed rail projects because almost all fail so there is a large enough sample to make statistical comparisons, whereas there can be no statistical analysis of successes because they are so rare.</li>
-			<li>Dominic Cummings - <a href="https://dominiccummings.com/2020/01/02/two-hands-are-a-lot-were-hiring-data-scientists-project-managers-policy-experts-assorted-weirdos/" target="_blank">"Two hands are a lot"</a></li>
 		</ul>
 	</section>
 
