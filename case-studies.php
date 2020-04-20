1f22ad50355b3a32334ebd8bd5207eeb2142bf50
John Appleseed <streaming040404@protonmail.com>
2020-04-20 20:16:40 +0200

    Update case-studies.php

--- case-studies.php
+++ case-studies.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width">
+	<meta name="robots" content="noindex,nofollow">
 	<title>Shinji Pons</title>
 	<link rel="preload" href="data/fonts/webfont.woff2" as="font" crossorigin="anonymous">
 	<style>
