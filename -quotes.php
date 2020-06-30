57d1201db4f3dccb54752bf04b26f8dd26e2da77
quickpoint <52743996+ItIjH7frYg@users.noreply.github.com>
2020-06-30 22:17:26 +0200

    first commit

--- -quotes.php
+++ -quotes.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	
+
 	<?php include '_head.php';?>
-	
+
 </head>
 <body>
 
 	<?php include '_nav.php';?>
-	
+
 	<section class="w2">
 		<h1>Interesting quotes</h1>
 	</section>
@@ -23,8 +23,8 @@
 	</section>
 	<section class="w4 no-rm">
 		<ul>
-			<li>The Oxford Handbook on Megaprojects points out that it is possible to quantify lessons from the failures of projects like high speed rail projects because almost all fail so there is a large enough sample to make statistical comparisons, whereas there can be no statistical analysis of successes because they are so rare.</li>
-		</ul>
+			<li>The Oxford Handbook on Megaprojects points out that it is possible to quantify lessons from the failures of projects like high speed rail projects because almost all fail so there is a large enough sample to make statistical comparisons, whereas there can be no statistical analysis of successes because they are so rare.</li
+		</ul
 	</section>
 
 	<?php include '_footer.php';?>
